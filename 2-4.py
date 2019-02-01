9d09fd0912b3a96f59b0ca35db68197d5fa7cd34
january14 <changcitto@gmail.com>
2019-02-01 22:26:29 +0900

    2장 연습문제 4번
    Signed-off-by: january14 <changcitto@gmail.com>

---
+++ 2-4.py
@@ -0,0 +1,13 @@
+print((5 > 4) and (3 == 5))     # True and False = False
+print(not (5 > 4))  # not(True) = False
+print((5 > 4) or (3 == 5))  # True or False = True
+print(not ((5 > 4) or (3 == 5)))    # not(True or False) = not(True) = False
+print((True and True) and (True == False))  # True and False = False
+print((not False) or (not True))    # True or False = True
+
+# False
+# False
+# True
+# False
+# False
+# True
