c49b1816a6d4f52ece3342f927e1c369974bacdf
january14 <changcitto@gmail.com>
2019-02-01 22:07:35 +0900

    2장 연습문제 12번
    Signed-off-by: january14 <changcitto@gmail.com>

---
+++ 2-12.py
@@ -0,0 +1,13 @@
+for i in range(10):
+    print(i)    # 0-9??
+print()
+
+for i in range(0, 10):
+    print(i)    # 0-9
+print()
+
+for i in range(0, 10, 1):
+    print(i)    # 0-9
+print()
+
+# 모두 같다.
