9f5dda1d575f32e68af2c01d6adc8dee31372b92
january14 <changcitto@gmail.com>
2019-02-04 01:58:38 +0900

    함수예제
    Signed-off-by: january14 <changcitto@gmail.com>

---
+++ chapter3/function1.py
@@ -0,0 +1,12 @@
+def hello():
+    print('Hello!')
+    print('Hello!!!')
+    print('Hello there')
+
+hello()
+
+def plusOne(number):
+    return number + 1
+
+newNumber = plusOne(5)
+print(newNumber)
